d216358d9eb68548531a00992c7376df73f80c14
ANTON <19111984@list.ru>
2022-08-11 20:32:17 +0700

    square of number

---
+++ Task16/Program.cs
@@ -0,0 +1,14 @@
+﻿//Написать программу, которая
+// на вход принимает 2 числа и
+// является ли оно квадратом другого
+Console.Write("Enter number1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b || b *b == a)
+{
+    Console.WriteLine("One number is the square of another number.");
+} else
+{
+    Console.WriteLine("Neither number is the square of the other.");
+}
