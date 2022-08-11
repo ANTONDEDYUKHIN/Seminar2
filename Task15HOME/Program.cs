54494199f2b021aa678b1b7f5f6b6488364da65b
ANTON <19111984@list.ru>
2022-08-11 23:29:39 +0700

    Weekend

---
+++ Task15HOME/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 15: Напишите программу, которая принимает 
+//на вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным. 6 -> да, 7 -> да, 1 -> нет
+Console.Clear();
+Console.Write("Enter int number: ");
+int a = Convert.ToInt32(System.Console.ReadLine());
+if( a == 1) System.Console.WriteLine("No");
+else if( a == 2) System.Console.WriteLine("No");
+else if( a == 3) System.Console.WriteLine("No");
+else if( a == 4) System.Console.WriteLine("No");
+else if( a == 5) System.Console.WriteLine("No");
+else if( a == 6) System.Console.WriteLine("Yes");
+else if( a == 7) System.Console.WriteLine("Yes");
+else System.Console.WriteLine("Number is not diapasone");
