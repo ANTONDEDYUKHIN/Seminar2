cd28743d15bfda08cf748ea4b24f6df37bad6e4a
ANTON <19111984@list.ru>
2022-08-13 20:35:25 +0700

    max dig of num

---
+++ Task09/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу, которая выводит случайное
+// число из отрезка [10, 99] и показывает
+// наибольшую цифру числа. 78 - 8, 12 - 2, 85 - 8.
+Console.Clear();
+int number = new Random().Next(10, 100);//10, 99+1
+Console.WriteLine($"Случайное число из диапазона 10- 99 =>{number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if (firstDigit == secondDigit)Console.WriteLine($"Числа равны.");
+else if(firstDigit > secondDigit)Console.WriteLine($"Наибольшая цифра числа {number} => {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа {number} => {secondDigit}");
