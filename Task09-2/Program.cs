5f32ed753c922dc5917938f629cd1f43719d1933
ANTON <19111984@list.ru>
2022-08-13 21:30:57 +0700

    method1

--- Task09-2/Program.cs
+++ Task09-2/Program.cs
@@ -11,7 +11,9 @@ int MaxDigit(int number)
 {
     int firstDigit = number / 10;
     int secondDigit = number % 10;
+    if (firstDigit == secondDigit) return 0;
     return firstDigit > secondDigit ? firstDigit : secondDigit;
 }
 int result = MaxDigit(number);
+string res = result == 0 ? "Цифры равны." : result.ToString();
 Console.WriteLine($"Наибольшая цифра числа {number} => {result}");
