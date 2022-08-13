5ddaa7c99790aaabef4e9972d8b8f519705be572
ANTON <19111984@list.ru>
2022-08-13 21:25:05 +0700

    method

---
+++ Task09-2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу, которая выводит случайное
+// число из отрезка [10, 99] и показывает
+// наибольшую цифру числа. 78 - 8, 12 - 2, 85 - 8.
+// Реализована через метод.
+Console.Clear();
+int number = new Random().Next(10, 100);//10, 99+1
+Console.WriteLine($"Случайное число из диапазона 10- 99 =>{number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+int MaxDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+int result = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа {number} => {result}");
