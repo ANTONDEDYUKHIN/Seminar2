c0c7c498ec6849833946b9f4ec963cce7e5e07cb
ANTON <19111984@list.ru>
2022-08-14 10:22:41 +0700

    2 цифра 3зн числа

--- Task10HOME/Program.cs
+++ Task10HOME/Program.cs
@@ -2,8 +2,12 @@
 //трёхзначное число и на выходе показывает вторую цифру этого
 // числа. Решить без использования строк. 456 -> 5
 //782 -> 8,  918 -> 1
+Console.Clear();
 int number = new Random().Next(100, 1000);
-int num1 = number / 10; //9
-int num2 = number / 100; //8
+Console.WriteLine($"Трехзначное число: {number}");
+int num1 = number / 10;
+Console.WriteLine($"Число 1: {num1}");
+int num2 = number / 100;
+Console.WriteLine($"Число 2: {num2}");
 int num3 = num1 - num2*10;
-Console.WriteLine($"Вывод трехзначного числа: {number} -> {num3}");
+Console.WriteLine($"Вторая цифра трехзначного числа: {number} -> {num3}");
